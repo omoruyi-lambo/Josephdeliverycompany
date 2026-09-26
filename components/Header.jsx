3283ea09a56b070e9fc1080cfda685a0a14afc89
Josephdeliverycompany <deploy@josephdeliverycompany.com>
2026-09-26 10:35:56 +0100

    fix: import useRouter in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { supabase } from '../lib/supabase/client';
 
 const NAV_LINKS = [
